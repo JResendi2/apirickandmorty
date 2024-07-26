e9ea7aa3b3818037e957dd7125d4e5be538687f5
JResendi2 <resendizj607@gmail.com>
2024-07-25 21:51:40 -0600

    fix: url de los asset

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
     */
 
     'url' => env('APP_URL', 'https://reediz.com'),
-    'url' => env('ASSET_URL', 'https://reediz-com.preview-domain.com/public/'),
+    'ASSET_URL' => env('ASSET_URL', 'https://reediz-com.preview-domain.com/public/'),
 
     /*
     |--------------------------------------------------------------------------
