7450d400c49496ec9c5ea2dc37380a85f54978a6
JResendi2 <resendizj607@gmail.com>
2024-08-26 08:54:56 -0600

    Correccion de la ruta de los estilos

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app-trivia.css',
+                'resources/css/app-chatbot.css',
                 'resources/css/normalize.css',
                 'resources/sass/welcome.scss',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/js/app-trivia.js',
+                'resources/js/app-chatbot.js',
             ],
             refresh: true,
         }),
